67f50f476d420fb33e4746af2321e9539006865c
Shreeyash Thorat <shreeyashsyt220801@gmail.com>
2023-08-28 01:16:47 +0530

    COMMIT

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const userRoutes = express.Router();
 const UserController = require('./../controllers/user_controller');
+const FrinedRequestController = require('./../controllers/friend_request_controller');
 
 userRoutes.post("/create-user", UserController.createUser);
 userRoutes.post("/login", UserController.login);
 userRoutes.post("/getUser/all", UserController.getAllUsers);
+
+// friend requests
+userRoutes.post("sendRequest", FrinedRequestController.friendRequest);
 
 module.exports = userRoutes;
